76c44f4b3196e52d31e455faeeac60058c5ad02e
awpwb <christian.froehlich@agfa.com>
2019-10-04 13:05:44 +0200

    refactor: setup as maven project

--- src/main/java/com/ssl/SSLCertificateExtractor.java
+++ src/main/java/com/ssl/SSLCertificateExtractor.java
@@ -1,3 +1,18 @@
+/*
+ *
+ *                   C O P Y R I G H T  (c) 2019
+ *      A G F A   H E A L T H C A R E   C O R P O R A T I O N
+ *                      All Rights Reserved
+ *
+ *
+ *          THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF
+ *                        AGFA CORPORATION
+ *         The copyright notice above does not evidence any
+ *        actual or intended publication of such source code.
+ */
+
+package com.ssl;
+
 import org.apache.commons.cli.*;
 
 import javax.net.ssl.*;
